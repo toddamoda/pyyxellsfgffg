+FIX fixed fitness calculation error because of different array types

pyxel/calibration/fitness.py
@@ -10,7 +10,11 @@ def sum_of_abs_residuals(simulated, target, weighting=None):
     :param weighting: np.array
     :return:
     """
-    diff = target - simulated
+    try:
+        diff = target.astype(float) - simulated.astype(float)
+    except AttributeError:
+        diff = float(target) - float(simulated)
+
     if weighting is not None:
         diff *= weighting
     return np.sum(np.abs(diff))
@@ -24,8 +28,12 @@ def sum_of_squared_residuals(simulated, target, weighting=None):
     :param weighting: np.array
     :return:
     """
-    diff = target - simulated
+    try:
+        diff = target.astype(float) - simulated.astype(float)
+    except AttributeError:
+        diff = float(target) - float(simulated)
     diff_square = diff * diff
+
     if weighting is not None:
         diff_square *= weighting
     return np.sum(diff_square)

pyxel/calibration/util.py
@@ -24,7 +24,7 @@ def read_data(data_path: t.Union[str, list]):
         elif '.npy' in data_path[i]:
             data = np.load(data_path[i])
         else:
-            data = np.loadtxt(data_path[i], dtype=float, delimiter='|')     # todo: more general with try-except
+            data = np.loadtxt(data_path[i], delimiter='|')     # todo: more general with try-except
         output += [data]
 
     return output
@@ -70,3 +70,23 @@ def check_ranges(target_fit_range: t.Optional[t.List[int]],
             for i in [2, 3]:
                 if target_fit_range[i] < 0 or target_fit_range[i] > cols:
                     raise ValueError('Value of target fit range is wrong')
+
+
+# def check_array_types(a, b):
+#     """TBW.
+#
+#     :param a: np
+#     :param b:
+#     :return:
+#     """
+#     if isinstance(a, np.ndarray) and isinstance(b, np.ndarray):
+#         if a.dtype and b.dtype in [np.int32, np.int64]:
+#             pass
+#         elif a.dtype and b.dtype in [np.float16, np.float32, np.float64]:
+#             pass
+#         elif a.dtype and b.dtype in [np.uint16, np.uint32, np.uint64]:
+#             pass
+#         else:
+#             raise TypeError('Numpy.ndarray objects should have the same type.')
+#     else:
+#         raise TypeError('Arrays should be numpy.ndarray objects.')

pyxel/physics/array.py
@@ -18,18 +18,17 @@ class Array:
     @property
     def array(self):
         """TBW."""
-        return self._array.astype(self.type)     # TODO
+        return self._array.astype(self.type)
 
     @array.setter
     def array(self, value):
         """TBW."""
-        if isinstance(value, np.ndarray):        # TODO
+        if isinstance(value, np.ndarray):
             if value.dtype in self.type_list:
                 self.type = value.dtype
             else:
                 raise TypeError('Type of %s array should be a(n) %s' %
                                 (self.__class__.__name__, self.exp_type.__name__))
-
             self._array = value
         else:
             raise TypeError('%s array should be a numpy.ndarray' % self.__class__.__name__)

pyxel/physics/image.py
@@ -11,7 +11,7 @@ cds.enable()
 
 
 class Image(Array):
-    """Image class defining and storing information of detector image."""
+    """Image class defining and storing information of detector image. Inherits from Array class."""
 
     def __init__(self, geo: Geometry) -> None:
         """TBW.
@@ -22,4 +22,3 @@ class Image(Array):
         self.exp_type = np.uint
         self.type_list = [np.uint16, np.uint32, np.uint64]
         self._array = np.zeros((geo.row, geo.col), dtype=self.exp_type)
-        # np.int8 results bitpix error from fitsfile

tests/unittests/test_fitting.py
@@ -107,18 +107,18 @@ def test_boundaries(yaml):
 
 @pytest.mark.parametrize('simulated_data, target_data, expected_fitness',
                          [
-                             (231, 231, (0, 0)),
-                             (231, 145, (86, 7396)),
-                             (2.31, 1.45, (0.8600000000000001, 0.7396000000000001)),
-                             (2., 1, (1.0, 1.0)),
-                             (np.array([1, 9, 45, 548, 2, 2]), np.array([1, 9, 45, 548, 2, 2]), (0, 0)),
-                             (np.array([1, 9, 45, 548, 2, 2]), np.array([1, 3, 56, 21, 235, 11]), (786, 332256)),
+                             (231, 231, 0.),
+                             (231, 145, 86.),
+                             (2.31, 1.45, 0.8600000000000001),
+                             (2., 1, 1.0),
+                             (np.array([1, 9, 45, 548, 2, 2]), np.array([1, 9, 45, 548, 2, 2]), 0.),
+                             (np.array([1, 9, 45, 548, 2, 2]), np.array([1, 3, 56, 21, 235, 11]), 786.),
                              (np.array([[1362., 1378.], [1308., 1309.]]),
-                              np.array([[1362., 1378.], [1308., 1309.]]), (0, 0)),
+                              np.array([[1362., 1378.], [1308., 1309.]]), 0.),
                              (np.array([[1362., 1378.],
                                         [1308., 1309.]]),
                               np.array([[1462., 1368.],
-                                        [1508., 1399.]]), (400.0, 58200.0))
+                                        [1508., 1399.]]), 400.)
                           ])
 def test_calculate_fitness(simulated_data, target_data, expected_fitness):
     """Test"""
@@ -128,7 +128,7 @@ def test_calculate_fitness(simulated_data, target_data, expected_fitness):
     mf = ModelFitting(processor)
     configure(mf, simulation)
     fitness = mf.calculate_fitness(simulated_data, target_data)
-    assert fitness == expected_fitness[0]
+    assert fitness == expected_fitness
     print('fitness: ', fitness)
 
 
@@ -182,8 +182,7 @@ def test_custom_fitness(yaml, simulated_data, target_data, expected_fitness):
                              ('tests/data/calibrate_models.yaml',
                               np.array([1., 0.5, 1.5, -2., -3., 4.5, -4., 1.,
                                         0.5, -3.5, 2., -3., -4., 0.5, 1., 100.]),
-                              # 185720.6490372545)
-                              5131.350962745482)
+                              5180.350962745482)
                           ])
 def test_fitness(yaml, parameter, expected_fitness):
     """Test"""