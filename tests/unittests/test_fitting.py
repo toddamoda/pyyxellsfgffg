8a759bac29882e9832460aec141661c16228b3ba
David Lucsanyi <david.lucsanyi@esa.int>
2018-12-20 18:21:27 +0100

    +FIX fixed test

--- tests/unittests/test_fitting.py
+++ tests/unittests/test_fitting.py
@@ -182,7 +182,7 @@ def test_custom_fitness(yaml, simulated_data, target_data, expected_fitness):
                              ('tests/data/calibrate_models.yaml',
                               np.array([1., 0.5, 1.5, -2., -3., 4.5, -4., 1.,
                                         0.5, -3.5, 2., -3., -4., 0.5, 1., 100.]),
-                              2852.6490372545177)
+                              5180.350962745482)
                           ])
 def test_fitness(yaml, parameter, expected_fitness):
     """Test"""
