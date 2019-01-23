8d110ae7c25db90584d95886118b98b23ad1cbb0
David Lucsanyi <david.lucsanyi@esa.int>
2019-01-23 11:29:05 +0100

    +CHG finished poppy wrapper

--- pyxel/models/optics/poppy.py
+++ pyxel/models/optics/poppy.py
@@ -1,7 +1,6 @@
 """Pyxel photon generator models."""
 import logging
 import poppy as op
-import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
 # import pyxel
@@ -10,35 +9,59 @@ from pyxel.detectors.detector import Detector
 
 # @pyxel.validate
 # @pyxel.argument(name='image_file', label='', validate=check_path)
-def poppy(detector: Detector):
+def optical_psf(detector: Detector,
+                wavelength: float,
+                pixelscale: float,
+                fov_pixels: int,
+                optical_system,
+                fov_arcsec: float = None
+                ):
     """POPPY (Physical Optics Propagation in PYthon) model wrapper.
 
     https://poppy-optics.readthedocs.io/en/stable/index.html
 
     :param detector: Pyxel Detector object
+    :param wavelength:
+    :param pixelscale:
+    :param fov_pixels: Field Of View on detector plane in pixels
+    :param fov_arcsec: Field Of View on detector plane in arcsec
+    :return:
     """
     logging.info('')
-    # geo = detector.geometry
 
-    radius1 = 1    # meter
-    radius2 = 3    # meter
+    if fov_arcsec:                      # TODO
+        raise NotImplementedError
 
-    nwaves = 1   # defocus
+    osys = op.OpticalSystem(npix=1000)  # default: 1024
 
-    # fov_arcsec = 10.
-    fov_pixels = 30      # FOV: array with shape 2 pix x 2 pix
-    pixelscale = 0.1
-
-    wavelength = 2e-6   # wavelength in microns
-
-    osys = op.OpticalSystem(npix=1000)      # TODO: this should be: max(detector.photon.array.shape)
-
-    osys.add_pupil(op.CircularAperture(radius=radius1))
-
-    # osys.add_pupil(op.CircularAperture(radius=radius2))
-    osys.add_pupil(op.ThinLens(nwaves=nwaves,
-                               reference_wavelength=wavelength,
-                               radius=radius2))
+    for item in optical_system:
+        if item['item'] == 'CircularAperture':
+            optical_item = op.CircularAperture(radius=item['radius'])
+        elif item['item'] == 'ThinLens':
+            optical_item = op.ThinLens(nwaves=item['nwaves'],
+                                       reference_wavelength=wavelength,
+                                       radius=item['radius'])
+        elif item['item'] == 'SquareAperture':
+            optical_item = op.SquareAperture(size=item['size'])
+        elif item['item'] == 'RectangularAperture':
+            optical_item = op.RectangleAperture(width=item['width'], height=item['height'])  # m
+        elif item['item'] == 'HexagonAperture':
+            optical_item = op.HexagonAperture(side=item['side'])
+        elif item['item'] == 'SecondaryObscuration':
+            optical_item = op.SecondaryObscuration(secondary_radius=item['secondary_radius'],
+                                                   n_supports=item['n_supports'],
+                                                   support_width=item['support_width'])      # cm
+        elif item['item'] == 'ZernikeWFE':
+            optical_item = op.ZernikeWFE(radius=item['radius'],
+                                         coefficients=item['coefficients'],         # list of floats
+                                         aperture_stop=item['aperture_stop'])       # bool
+        elif item['item'] == 'SineWaveWFE':
+            optical_item = op.SineWaveWFE(spatialfreq=item['spatialfreq'],      # 1/m
+                                          amplitude=item['amplitude'],          # um
+                                          rotation=item['rotation'])
+        else:
+            raise NotImplementedError
+        osys.add_pupil(optical_item)
 
     osys.add_detector(pixelscale=pixelscale,
                       # fov_arcsec=None,
@@ -48,50 +71,9 @@ def poppy(detector: Detector):
                         return_intermediates=True,
                         # return_final=True,
                         display_intermediates=True,
-                        normalize="last")
+                        normalize="last")                   # TODO NORMALIZATION!!!!
 
-    plt.figure()
-    plt.subplot(3, 3, 1, title='amplitude 0')
-    plt.imshow(psf[1][0].amplitude, cmap='gray')
-    plt.subplot(3, 3, 4, title='amplitude 1')
-    plt.imshow(psf[1][1].amplitude, cmap='gray')
-    plt.subplot(3, 3, 7, title='amplitude 2')
-    plt.imshow(psf[1][2].amplitude, cmap='gray')
-
-    plt.subplot(3, 3, 2, title='phase 0')
-    plt.imshow(psf[1][0].phase)
-    plt.subplot(3, 3, 5, title='phase 1')
-    plt.imshow(psf[1][1].phase)
-    plt.subplot(3, 3, 8, title='phase 2')
-    plt.imshow(psf[1][2].phase)
-
-    plt.subplot(3, 3, 3, title='intensity 0')
-    plt.imshow(psf[1][0].intensity, cmap='gray')
-    plt.subplot(3, 3, 6, title='intensity 1')
-    plt.imshow(psf[1][1].intensity, cmap='gray')
-    plt.subplot(3, 3, 9, title='intensity 2')
-    plt.imshow(psf[1][2].intensity, cmap='gray')
-
-    # plt.figure()
-    # op.display_psf(psf[0], title='display_psf')
-
-    plt.figure()
-    plt.imshow(np.log(psf[1][2].intensity))
-
-
-    # from scipy import misc
-    # ascent = misc.ascent()
-    # scharr = np.array(
-    #     [[-3 - 3j, 0 - 10j, +3 - 3j],
-    #      [-10 + 0j, 0 + 0j, +10 + 0j],
-    #      [-3 + 3j, 0 + 10j, +3 + 3j]])  # Gx + j*Gy
-    # grad = signal.convolve2d(ascent, scharr, mode='same', boundary='fill', fillvalue=0)
-
-    conv_with_wavefront = signal.convolve2d(detector.photons.array, psf[1][2].wavefront,
-                                            mode='same', boundary='fill', fillvalue=0)
-    conv_with_intensity = signal.convolve2d(detector.photons.array, psf[1][2].intensity,
-                                            mode='same', boundary='fill', fillvalue=0)
-
+    # psf[0][0].data == psf[1][-1].intensity
 
     plt.figure()
     ax_orig = plt.gca()
@@ -99,24 +81,56 @@ def poppy(detector: Detector):
     ax_orig.set_title('Original')
     ax_orig.set_axis_off()
 
+    # Convolution
+    detector.photons.array = signal.convolve2d(detector.photons.array, psf[0][0].data,
+                                               mode='same', boundary='fill', fillvalue=0)
+
     plt.figure()
     ax_int = plt.gca()
-    ax_int.imshow(conv_with_intensity, cmap='gray')
+    ax_int.imshow(detector.photons.array, cmap='gray')
     ax_int.set_title('Convolution with intensity')
     ax_int.set_axis_off()
 
-    plt.figure()
-    ax_mag = plt.gca()
-    ax_mag.imshow(np.absolute(conv_with_wavefront), cmap='gray')
-    ax_mag.set_title('Magnitude')
-    ax_mag.set_axis_off()
+    # plt.show()
 
-    plt.figure()
-    ax_ang = plt.gca()
-    ax_ang.imshow(np.angle(conv_with_wavefront), cmap='hsv')   # hsv is cyclic, like angles
-    ax_ang.set_title('Angle')
-    ax_ang.set_axis_off()
+    # conv_with_wavefront = signal.convolve2d(detector.photons.array, psf[1][2].wavefront,
+    #                                         mode='same', boundary='fill', fillvalue=0)
 
-    plt.show()
+    # plt.figure()
+    # plt.subplot(3, 3, 1, title='amplitude 0')
+    # plt.imshow(psf[1][0].amplitude, cmap='gray')
+    # plt.subplot(3, 3, 4, title='amplitude 1')
+    # plt.imshow(psf[1][1].amplitude, cmap='gray')
+    # plt.subplot(3, 3, 7, title='amplitude 2')
+    # plt.imshow(psf[1][2].amplitude, cmap='gray')
+    #
+    # plt.subplot(3, 3, 2, title='phase 0')
+    # plt.imshow(psf[1][0].phase)
+    # plt.subplot(3, 3, 5, title='phase 1')
+    # plt.imshow(psf[1][1].phase)
+    # plt.subplot(3, 3, 8, title='phase 2')
+    # plt.imshow(psf[1][2].phase)
+    #
+    # plt.subplot(3, 3, 3, title='intensity 0')
+    # plt.imshow(psf[1][0].intensity, cmap='gray')
+    # plt.subplot(3, 3, 6, title='intensity 1')
+    # plt.imshow(psf[1][1].intensity, cmap='gray')
+    # plt.subplot(3, 3, 9, title='intensity 2')
+    # plt.imshow(psf[1][2].intensity, cmap='gray')
 
-    pass
+    # plt.figure()
+    # op.display_psf(psf[0], title='display_psf')
+    # plt.figure()
+    # plt.imshow(np.log(psf[1][2].intensity))
+
+    # plt.figure()
+    # ax_mag = plt.gca()
+    # ax_mag.imshow(np.absolute(conv_with_wavefront), cmap='gray')
+    # ax_mag.set_title('Magnitude')
+    # ax_mag.set_axis_off()
+    #
+    # plt.figure()
+    # ax_ang = plt.gca()
+    # ax_ang.imshow(np.angle(conv_with_wavefront), cmap='hsv')   # hsv is cyclic, like angles
+    # ax_ang.set_title('Angle')
+    # ax_ang.set_axis_off()
--- pyxel/models/photon_generation/illumination.py
+++ pyxel/models/photon_generation/illumination.py
@@ -1,6 +1,7 @@
 """Pyxel photon generator models."""
 import logging
 import numpy as np
+import typing as t
 import pyxel
 from pyxel import check_type, check_choices
 from pyxel.detectors.detector import Detector
@@ -8,15 +9,15 @@ from pyxel.detectors.detector import Detector
 
 @pyxel.validate
 @pyxel.argument(name='level', label='number of photons', units='', validate=check_type(int))
-@pyxel.argument(name='option', label='type of illumination', units='', validate=check_choices(['uniform',
-                                                                                               'rectangular_mask',
-                                                                                               'elliptic_mask']))
+@pyxel.argument(name='option', label='type of illumination', units='',
+                validate=check_choices(['uniform', 'rectangular_mask', 'elliptic_mask']))
 # @pyxel.argument(name='size', label='size of 2d array', units='', validate=check_type(list))
+# @pyxel.argument(name='mask_size', label='size of mask', units='', validate=check_type(list))
 def illumination(detector: Detector,
                  level: int,
                  option: str = 'uniform',   # todo: remove default arg.
                  size: list = None,
-                 mask_size: list = None):
+                 mask_size: t.List[int] = None):
     """Generate photons (uniform illumination).
 
     :param detector: Pyxel Detector object
@@ -40,23 +41,29 @@ def illumination(detector: Detector,
     if option == 'uniform':
         photon_array = np.ones(shape, dtype=int) * level
     elif option == 'rectangular_mask':
-        photon_array = np.zeros(shape, dtype=int)
-        a = int((shape[0]-mask_size[0]) / 2)
-        b = int((shape[1]-mask_size[1]) / 2)
-        photon_array[slice(a, a + mask_size[0]), slice(b, b + mask_size[1])] = level
+        if mask_size:
+            photon_array = np.zeros(shape, dtype=int)
+            a = int((shape[0]-mask_size[0]) / 2)
+            b = int((shape[1]-mask_size[1]) / 2)
+            photon_array[slice(a, a + mask_size[0]), slice(b, b + mask_size[1])] = level
+        else:
+            raise ValueError('mask_size argument should be defined for illumination model')
     elif option == 'elliptic_mask':
-        photon_array = np.zeros(shape, dtype=int)
-        h = shape[0]                # TODO
-        w = shape[1]                # TODO
-        center = None               # TODO
-        radius = mask_size[0]       # TODO
-        if center is None:  # use the middle of the image
-            center = [int(w / 2), int(h / 2)]
-        if radius is None:  # use the smallest distance between the center and image walls
-            radius = min(center[0], center[1], w - center[0], h - center[1])
-        y, x = np.ogrid[:h, :w]
-        dist_from_center = np.sqrt((x - center[0]) ** 2 + (y - center[1]) ** 2)
-        photon_array[dist_from_center <= radius] = level
+        if mask_size:
+            photon_array = np.zeros(shape, dtype=int)
+            h = shape[0]                # TODO
+            w = shape[1]                # TODO
+            center = None               # TODO
+            radius = mask_size[0]       # TODO
+            if center is None:  # use the middle of the image
+                center = [int(w / 2), int(h / 2)]
+            if radius is None:  # use the smallest distance between the center and image walls
+                radius = min(center[0], center[1], w - center[0], h - center[1])
+            y, x = np.ogrid[:h, :w]
+            dist_from_center = np.sqrt((x - center[0]) ** 2 + (y - center[1]) ** 2)
+            photon_array[dist_from_center <= radius] = level
+        else:
+            raise ValueError('mask_size argument should be defined for illumination model')
     else:
         raise NotImplementedError
 
