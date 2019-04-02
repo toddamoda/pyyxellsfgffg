d6d50167ed9176b377102f1dbbe70646191cd2b7
Benoît Serra <benoit.serra86@gmail.com>
2019-04-02 17:35:43 +0200

    +ADD Adding ideal SAR ADC behavior for digitizing the data

--- pyxel/models/readout_electronics/digitization.py
+++ pyxel/models/readout_electronics/digitization.py
@@ -46,3 +46,37 @@ def simple_processing(detector: Detector):
     logger.info('')
     detector.signal.array *= detector.characteristics.a2
     detector.image.array = detector.signal.array
+
+def SAR_ADC(detector: Detector,
+            adc_bits: int = 16,
+            range_volt: tuple = (0,5)):
+    import numpy as np
+    """Digitize signal array using SAR ADC logic.
+
+    :param detector: Pyxel Detector object
+    :param bits: integer: Number of bits for the ADC
+    :param range_volt: tuple with min anx max volt value
+    """
+    # Extract the data to digitize
+    data = detector.signal.array
+    data_digitized = np.zeros((detector.geometry.row, detector.geometry.col))
+    # First normalize the data to voltage since there is no model for
+    # the conversion photogenerated carrier > volts in the model/charge_measure
+    data = data*range_volt[1]/np.max(data)
+    #steps = list()
+    # Set the reference voltage of the ADC to half the max
+    ref = range_volt[1]/2.
+    # For each bits, compare the value of the ref to the capacitance value
+    for i in np.arange(0,adc_bits):
+        # digital value associated with this step
+        digital_value = 2**(adc_bits-(i+1))
+        # All data that is higher than the ref is equal to the dig. value
+        data_digitized[data>=ref] += digital_value
+        # Subtract ref value from the data
+        data[data>=ref] -= ref
+        #steps.append(ref)
+        # Divide reference voltage by 2 for next step
+        ref /= 2.
+    
+    detector.image.array = data_digitized
+    #return data_digitized, steps
