a10159460c01bbf1a144a6ea406dffbbe53a1d3b
David Lucsanyi <lucsanyid@gmail.com>
2019-05-21 16:48:45 +0200

    +FIX fixed conflict: pyxel_yaml_loader func moved to and imported from pyxel.io module

--- pyxel/__init__.py
+++ pyxel/__init__.py
@@ -1,15 +1,10 @@
 """Pyxel detector simulation framework."""
 import os
-# import logging
 import typing as t
 import esapy_config.checkers as checkers
 import esapy_config.validators as validators
 import esapy_config.funcargs as funcargs
 import esapy_config.config as config
-import esapy_config.io as io
-# from pyxel.detectors.detector import Detector
-# from pyxel.detectors.ccd import CCD
-# from pyxel.detectors.cmos import CMOS
 
 
 __all__ = ['models',
@@ -118,61 +113,3 @@ def validate_type(att_type, is_optional: bool = False):
     """TBW."""
     return validators.validate_type(att_type=att_type,
                                     is_optional=is_optional)
-
-
-def pyxel_yaml_loader():
-    """TBW."""
-    from pyxel.parametric.parametric import Configuration
-    from pyxel.parametric.parametric import ParametricAnalysis
-    from pyxel.parametric.parameter_values import ParameterValues
-    from pyxel.calibration.calibration import Calibration
-    from pyxel.calibration.calibration import Algorithm
-    from pyxel.pipelines.model_function import ModelFunction
-    from pyxel.pipelines.model_group import ModelGroup
-    from pyxel.util import Outputs
-
-    # io.ObjectModelLoader.add_class_ref(['detector', 'class'])
-    # io.ObjectModelLoader.add_class_ref(['detector', None, 'class'])
-    # io.ObjectModelLoader.add_class_ref(['pipeline', 'class'])
-
-    from pyxel.detectors import CCD, CMOS, Detector
-    from pyxel.detectors import Geometry, Characteristics, Material, Environment
-    from pyxel.detectors.ccd import CCDGeometry, CCDCharacteristics
-    from pyxel.detectors.cmos import CMOSGeometry, CMOSCharacteristics
-
-    from pyxel.pipelines.pipeline import DetectionPipeline
-
-    io.ObjectModelLoader.add_class(CCD, ['ccd_detector'])  # pyxel.detectors.ccd.CCD
-    io.ObjectModelLoader.add_class(CMOS, ['cmos_detector'])
-    io.ObjectModelLoader.add_class(Detector, ['detector'])
-
-    io.ObjectModelLoader.add_class(CCDGeometry, ['ccd_detector', 'geometry'])
-    io.ObjectModelLoader.add_class(CMOSGeometry, ['cmos_detector', 'geometry'])
-    io.ObjectModelLoader.add_class(Geometry, ['detector', 'geometry'])
-
-    io.ObjectModelLoader.add_class(CCDCharacteristics, ['ccd_detector', 'characteristics'])
-    io.ObjectModelLoader.add_class(CMOSCharacteristics, ['cmos_detector', 'characteristics'])
-    io.ObjectModelLoader.add_class(Characteristics, ['detector', 'characteristics'])
-
-    io.ObjectModelLoader.add_class(Material, [None, 'material'])
-    io.ObjectModelLoader.add_class(Environment, [None, 'environment'])
-
-    io.ObjectModelLoader.add_class(DetectionPipeline, ['pipeline'])
-
-    io.ObjectModelLoader.add_class(ModelGroup, ['pipeline', None])
-    io.ObjectModelLoader.add_class(ModelFunction, ['pipeline', None, None])
-
-    io.ObjectModelLoader.add_class(Configuration, ['simulation'])
-
-    io.ObjectModelLoader.add_class(Outputs, ['simulation', 'outputs'])
-
-    io.ObjectModelLoader.add_class(ParametricAnalysis, ['simulation', 'parametric'])
-    io.ObjectModelLoader.add_class(ParameterValues, ['simulation', 'parametric', 'parameters'], is_list=True)
-
-    io.ObjectModelLoader.add_class(Calibration, ['simulation', 'calibration'])
-    io.ObjectModelLoader.add_class(Algorithm, ['simulation', 'calibration', 'algorithm'])
-    io.ObjectModelLoader.add_class(ModelFunction, ['simulation', 'calibration', 'fitness_function'])
-    io.ObjectModelLoader.add_class(ParameterValues, ['simulation', 'calibration', 'parameters'], is_list=True)
-
-
-pyxel_yaml_loader()
