b54a1d86dec677602f395ac5c92ea05c70ff215a
Brad Kelman <braderz107@btinternet.com>
2021-03-30 15:48:22 +0100

    Added to arcic py file and adding a test directory for now

--- pyxel/models/charge_transfer/arctic.py
+++ pyxel/models/charge_transfer/arctic.py
@@ -8,24 +8,23 @@ More information:
 https://pypi.org/project/arcticpy/
 """
 
-
-import arcticpy as ac
+import arcticpy.arcticpy as ac
 from pyxel.detectors import CCD
 import numpy as np
 import typing as t
 
+
 # CCD, ROE, and trap species parameters
 
 
-
 def arctic(detector: CCD,
-            quadrant_letter: str,
-            well_fill_power: float,
-            density: float,
-            release_timescale: t.Sequence[float]) -> np.array:
-
+           well_fill_power: float,
+           density: float,
+           release_timescale: t.Sequence[float]) -> np.array:
     char = detector.characteristics
-    image = ac.acs.FrameACS.from_fits(file_path=detector.pixel.array, quadrant_letter=quadrant_letter)
+    image = detector.pixel.array
+    image = image.astype(float)
+    # image = ac.acs.FrameACS.from_fits(file_path=detector.pixel.array, quadrant_letter=quadrant_letter)
     ccd = ac.CCD(well_fill_power=well_fill_power, full_well_depth=char.fwc)
     roe = ac.ROE()
     trap = ac.Trap(density=-density, release_timescale=release_timescale)
@@ -36,3 +35,4 @@ def arctic(detector: CCD,
         parallel_roe=roe,
     )
     return image_cti_added
+
---
+++ test_arcticpy/testing_arcitc.py
@@ -0,0 +1,8 @@
+import os
+print("start test")
+print(os.getcwd())
+os.system('pyxel --config test_arctic_pipeline.yaml')
+print("end test")
+
+
+
