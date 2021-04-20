37a346342d0de5f68d130ed2ffbdacc4b5ad4d91
Brad <braderz107@btinternet.com>
2021-04-20 16:46:09 +0100

    Cleaned up arctic python file, removed unnecessary imports and unused code

--- pyxel/models/charge_transfer/arctic.py
+++ pyxel/models/charge_transfer/arctic.py
@@ -8,39 +8,29 @@ More information:
 https://pypi.org/project/arcticpy/
 """
 
-
-from arcticpy import main as ac
-from arcticpy import ccd as accd
-from arcticpy.roe import ROE
-from arcticpy import traps as trp
 import arcticpy as ac
+
+
 from pyxel.detectors import CCD
 import numpy as np
 import typing as t
 
 
-# CCD, ROE, and trap species parameters
-
-
 def arctic(detector: CCD,
            well_fill_power: float,
            density: float,
-           release_timescale: t.Sequence[float]) -> np.array:
+           release_timescale: t.Sequence[float]) -> np.ndarray:
     char = detector.characteristics
     image = detector.pixel.array
     image = image.astype(float)
-    # image = ac.acs.FrameACS.from_fits(file_path=detector.pixel.array, quadrant_letter=quadrant_letter)
-    ccd = accd.CCD(well_fill_power=well_fill_power, full_well_depth=char.fwc)
-    roe = ROE()
-    #ccd = ac.CCD(well_fill_power=well_fill_power, full_well_depth=char.fwc)
-    #roe = ac.ROE()
-    trap = trp.Trap(density=-density, release_timescale=release_timescale)
-    image_cti_added = ac.add_cti(
+    ccd = ac.CCD(well_fill_power=well_fill_power, full_well_depth=char.fwc)
+    roe = ac.ROE()
+    trap = ac.Trap(density=density, release_timescale=release_timescale)
+    s = ac.add_cti(
         image=image,
         parallel_traps=[trap],
         parallel_ccd=ccd,
         parallel_roe=roe,
     )
-    return image_cti_added
+    return s
 
-#Try to work out the imports better for the afternoon - struggling to get this stupid thing to imprt the things I want it to!!!!!!!!
