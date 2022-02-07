340aff533f4f5239424a31ec92d149acaa2c3782
Enrico Biancalani <ebiancalani94@gmail.com>
2022-02-07 04:21:29 +0000

    Update pyxel/models/phasing/pulse_processing.py, docs/source/references/model_groups/phasing_models.rst

--- pyxel/models/phasing/pulse_processing.py
+++ pyxel/models/phasing/pulse_processing.py
@@ -7,6 +7,7 @@
 
 """Phase-pulse processing model."""
 
+import astropy.constants as const
 import numpy as np
 
 from pyxel.detectors import MKID
@@ -18,12 +19,11 @@ def convert_to_phase(
     responsivity: float,
     scaling_factor: float = 2.5e2,
 ) -> np.ndarray:
-    """Convert an array of charge into an array of phases.
+    """Convert an array of charges into an array of phase pulses.
 
     Parameters
     ----------
     array: ndarray
-    wavelength: float
     responsivity: float
     scaling_factor: float
 
@@ -31,14 +31,13 @@ def convert_to_phase(
     -------
     ndarray
     """
-    if not wavelength > 0:
-        raise ValueError("Only positive values accepted for wavelength.")
     if not scaling_factor > 0:
         raise ValueError("Only positive values accepted for scaling_factor.")
     if not responsivity > 0:
         raise ValueError("Only positive values accepted for responsivity.")
 
-    output = array * wavelength * scaling_factor / responsivity
+    output = array * responsivity * scaling_factor
+
     return output.astype("float64")
 
 
@@ -57,7 +56,7 @@ def pulse_processing(
     detector: MKID
         Pyxel :term:`MKID` detector object.
     wavelength: float
-        Wavelength.
+        Wavelength [μm].
     responsivity: float
         Responsivity of the pixel.
     scaling_factor: float
@@ -66,7 +65,9 @@ def pulse_processing(
         the quasi-particle losses, etc.
     """
     if not isinstance(detector, MKID):
-        raise TypeError("Expecting a MKID object for the detector.")
+        raise TypeError("Expecting an MKID object for the detector.")
+    if not wavelength > 0:
+        raise ValueError("Only positive values accepted for wavelength.")
 
     detector.phase.array = convert_to_phase(
         array=detector.charge.array,
@@ -74,3 +75,21 @@ def pulse_processing(
         responsivity=responsivity,
         scaling_factor=scaling_factor,
     )
+
+    ## IN FIERI, for a single pixel:
+
+    # Boltzmann's constant [J K^-1]
+    boltzmann_cst: float = const.k_B.value
+
+    t_c = 1.26 # Hardcoded; used also in /pyxel/models/readout_electronics/dead_time.py
+    delta = 1.76 * boltzmann_cst * t_c # Used also in /pyxel/models/readout_electronics/dead_time.py
+
+    R = np.sqrt(0.57 * 6.62607004 * 1.e-34 * 299792458. / (wavelength * 1.e-6 * 0.2 * delta)) / (2. * np.sqrt(2. * np.log(2.)))
+
+    sigma_lambda = wavelength / R / (2 * np.sqrt(2 * np.log(2)))
+
+    mu, sigma = wavelength, sigma_lambda
+
+    np.random.seed(42)
+
+    Gaussian_samples = np.random.normal(mu, sigma, detector.phase.array) 
