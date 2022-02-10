b9024e3d6135c0f80e76e7cd5ca42b3ef3f62aab
Enrico Biancalani <ebiancalani94@gmail.com>
2022-02-10 04:20:01 +0000

    Update pyxel/models/phasing/pulse_processing.py

--- pyxel/models/phasing/pulse_processing.py
+++ pyxel/models/phasing/pulse_processing.py
@@ -15,7 +15,6 @@ from pyxel.detectors import MKID
 
 def convert_to_phase(
     array: np.ndarray,
-    wavelength: float,
     responsivity: float,
     scaling_factor: float = 2.5e2,
 ) -> np.ndarray:
@@ -31,9 +30,9 @@ def convert_to_phase(
     -------
     ndarray
     """
-    if not scaling_factor > 0:
+    if not scaling_factor > 0.:
         raise ValueError("Only positive values accepted for scaling_factor.")
-    if not responsivity > 0:
+    if not responsivity > 0.:
         raise ValueError("Only positive values accepted for responsivity.")
 
     output = array * responsivity * scaling_factor
@@ -46,6 +45,9 @@ def pulse_processing(
     wavelength: float,
     responsivity: float,
     scaling_factor: float = 2.5e2,
+    t_c: float = 1.26, 
+    eta_pb: float = 0.59,
+    F: float = 0.2,
 ) -> None:
     """Phase-pulse processor.
 
@@ -56,17 +58,23 @@ def pulse_processing(
     detector: MKID
         Pyxel :term:`MKID` detector object.
     wavelength: float
-        Wavelength [μm].
+        Wavelength. Unit: um
     responsivity: float
         Responsivity of the pixel.
     scaling_factor: float
         Scaling factor taking into account the missing pieces of superconducting physics,
         as well as the resonator quality factor, the bias power,
         the quasi-particle losses, etc.
+    t_c : float [used also in /pyxel/models/readout_electronics/dead_time.py]
+        Material dependent critical temperature. Unit: K
+    eta_pb: float
+        Superconducting pair-breaking efficiency.
+    F: float
+        Fano's factor.
     """
     if not isinstance(detector, MKID):
         raise TypeError("Expecting an MKID object for the detector.")
-    if not wavelength > 0:
+    if not wavelength > 0.:
         raise ValueError("Only positive values accepted for wavelength.")
 
     detector.phase.array = convert_to_phase(
@@ -76,20 +84,25 @@ def pulse_processing(
         scaling_factor=scaling_factor,
     )
 
-    ## IN FIERI, for a single pixel:
+    ## IN FIERI:
 
     # Boltzmann's constant [J K^-1]
     boltzmann_cst: float = const.k_B.value
 
-    t_c = 1.26 # Hardcoded; used also in /pyxel/models/readout_electronics/dead_time.py
-    delta = 1.76 * boltzmann_cst * t_c # Used also in /pyxel/models/readout_electronics/dead_time.py
+    # Planck's constant [J s]
+    planck_cst: float = const.h.value
 
-    R = np.sqrt(0.57 * 6.62607004 * 1.e-34 * 299792458. / (wavelength * 1.e-6 * 0.2 * delta)) / (2. * np.sqrt(2. * np.log(2.)))
+    # Speed of light in vacuum [m s^-1]
+    c_cst: float = const.c.value
 
-    sigma_lambda = wavelength / R / (2 * np.sqrt(2 * np.log(2)))
+    delta = 1.76 * boltzmann_cst * t_c # [used also in /pyxel/models/readout_electronics/dead_time.py]
+
+    R = np.sqrt(eta_pb * planck_cst * c_cst / (wavelength * 1.e-6 * F * delta)) / (2. * np.sqrt(2. * np.log(2.)))
+
+    sigma_lambda = wavelength / (R * (2 * np.sqrt(2 * np.log(2))))
 
     mu, sigma = wavelength, sigma_lambda
 
     np.random.seed(42)
 
-    Gaussian_samples = np.random.normal(mu, sigma, detector.phase.array) 
+    Gaussian_samples = np.random.normal(mu, sigma, detector.phase.array[0][0]) # To be continued...
