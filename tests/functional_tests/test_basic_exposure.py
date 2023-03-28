c2ad0427a5bcdf446e8de8bbe60aae89fa295e56
frederic lemmel <frederic.lemmel@ext.esa.int>
2023-03-28 15:12:20 +0200

    Fix issues with 'Detector.to_asdf' and 'Detector.from_asdf' with 'ProcessedData'.

--- tests/functional_tests/test_basic_exposure.py
+++ tests/functional_tests/test_basic_exposure.py
@@ -14,7 +14,7 @@ from pyxel import Configuration
 from pyxel.detectors import Detector
 
 
-def test_basic_exposure(tmp_path: Path):
+def test_basic_exposure_hdf5(tmp_path: Path):
     """Functional test with a basic Exposure mode."""
     config_filename: str = "data/basic_exposure.yaml"
 
@@ -53,4 +53,46 @@ def test_basic_exposure(tmp_path: Path):
 
     # Load to a new 'detector' object from '.hdf5' file
     new_detector = Detector.from_hdf5(detector_filename)
+    assert detector == new_detector
+
+
+def test_basic_exposure_asdf(tmp_path: Path):
+    """Functional test with a basic Exposure mode."""
+    config_filename: str = "data/basic_exposure.yaml"
+
+    current_folder = Path(__file__).parent
+    config_full_filename = current_folder / config_filename
+    assert config_full_filename.exists()
+
+    # Read configuration file
+    cfg = pyxel.load(config_filename)
+    assert isinstance(cfg, Configuration)
+
+    # Save 'detector' before modifications
+    detector = cfg.detector
+    assert isinstance(detector, Detector)
+
+    detector_filename_before = tmp_path / "before_detector.asdf"
+    assert not detector_filename_before.exists()
+
+    detector.to_asdf(detector_filename_before)
+    assert detector_filename_before.exists()
+
+    # Execute 'cfg'
+    ds = pyxel.run_mode(
+        mode=cfg.running_mode,
+        detector=cfg.detector,
+        pipeline=cfg.pipeline,
+    )
+    assert isinstance(ds, xr.Dataset)
+
+    # Save the 'detector' object into a '.asdf' file
+    detector_filename: Path = tmp_path / "detector.asdf"
+    assert not detector_filename.exists()
+
+    detector.to_asdf(detector_filename)
+    assert detector_filename.exists()
+
+    # Load to a new 'detector' object from '.asdf' file
+    new_detector = Detector.from_asdf(detector_filename)
     assert detector == new_detector
