f3e910098539418571f7671e0550dabfbb5f5230
frederic lemmel <frederic.lemmel@ext.esa.int>
2023-04-04 16:40:41 +0200

    Refactoring.

--- pyxel/models/scene_generation/ariel_airs.py
+++ pyxel/models/scene_generation/ariel_airs.py
@@ -1,77 +1,93 @@
 """Pyxel photon generator models."""
-from typing import Literal, Optional, Sequence, Tuple
+from typing import Optional
 
 import matplotlib.pyplot as plt
 import numpy as np
-from wavelength_dependence_calculation import *
 
 from pyxel.detectors import Detector
+
+from .ariel_airs_calculation import (
+    convert_flux,
+    integrate_flux,
+    project_psfs,
+    read_psf_from_fits_file,
+    read_star_flux_from_file,
+)
 
 
 # ------------------------------------------------------
 def wavelength_dependence_airs(
+    detector: Optional[Detector] = None,
+    psf_filename: str = "CH1_big_cube_PSFs.fits",
+    target_filename: str = "target_flux_SED_00005.dat",
     time_scale: float = 1.0,
-    psf_filename="/Users/",
-    target_filename="/Users/",
 ) -> None:
-    """Generate the photon over the array according to a specific dispersion pattern (ARIEL-AIRS)
+    """Generate the photon over the array according to a specific dispersion pattern (ARIEL-AIRS).
 
     Parameters
     ----------
     detector : Detector
         Pyxel Detector object.
-    time_scale : float
-        Time scale of the photon flux, default is 1 second. 0.001 would be ms.
     psf_filename: string
-        The location and the filename where the PSFs are located
+        The location and the filename where the PSFs are located.
     target_filename: string
-        The location and the filename of the target file used in the simulation
+        The location and the filename of the target file used in the simulation.
     """
     # Extract information from the PSF
     psf_datacube, psf_wavelength, line_psf_pos, col_psf_pos = read_psf_from_fits_file(
-        psf_filename
+        filename=psf_filename
     )
-    plt.figure()
-    plt.pcolormesh(psf_datacube[10, :, :])
-    plt.title(psf_wavelength[10])
+    # plt.figure()
+    # plt.pcolormesh(psf_datacube[10, :, :])
+    # plt.title(psf_wavelength[10])
+
     # Read flux from the fits file
-    target_wavelength, target_flux = read_star_flux_from_file(target_filename)
+    target_wavelength, target_flux = read_star_flux_from_file(filename=target_filename)
+
     # convert the flux by multiplying by the area of the detector
     telescope_diameter_m1, telescope_diameter_m2 = (
         1.1,
         0.7,
     )  # in m, TODO to be replaced by Telescope Class ?
+
     target_conv_flux = convert_flux(
-        target_wavelength, target_flux, telescope_diameter_m1, telescope_diameter_m2
+        wavelength=target_wavelength,
+        flux=target_flux,
+        telescope_diameter_m1=telescope_diameter_m1,
+        telescope_diameter_m2=telescope_diameter_m2,
     )
     # Integrate the flux over the PSF spectral bin
     integrated_flux = integrate_flux(
         target_wavelength, target_conv_flux, psf_wavelength
     )  # The flux is now sample similarly to the PSF
+
     # The Flux can be multiplied here by the optical elements
-    # multiply_by_transmission(psf, transmission_dict) #TODO add class to take into account the transmission of the instrument
+    # multiply_by_transmission(psf, transmission_dict)
+    # #TODO add class to take into account the transmission of the instrument
     # Project the PSF onto the Focal Plane, to get the detector image
     row, col = 130, 64  # Could be replaced
     expend_factor = 18  # Expend factor used
     photon_incident, photo_electron_generated = project_psfs(
-        psf_datacube,
-        line_psf_pos,
-        col_psf_pos,
-        integrated_flux,
-        row,
-        col,
-        expend_factor,
+        psf_datacube=psf_datacube,
+        line_psf_pos=line_psf_pos,
+        col_psf_pos=col_psf_pos,
+        flux=integrated_flux,
+        row=row,
+        col=col,
+        expend_factor=expend_factor,
     )
     # Add the result to the photon array structure
     time_step = 1.0
-    photon_array = photo_electron_generated * (time_step / time_scale)
 
-    # try:
-    #    detector.photon.array += photon_array
-    # except ValueError as ex:
-    #    raise ValueError("Shapes of arrays do not match") from ex
+    if detector:
+        photon_array = photo_electron_generated * (time_step / time_scale)
 
-    ##### PLOT, to be deleted when implemented in Pyxel
+        try:
+            detector.photon.array += photon_array
+        except ValueError as ex:
+            raise ValueError("Shapes of arrays do not match") from ex
+
+    # PLOT, to be deleted when implemented in Pyxel
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.plot(target_wavelength, target_flux)
@@ -98,8 +114,10 @@ if __name__ == "__main__":
     psf_filename = "CH1_big_cube_PSFs.fits"
     target_filename = "Noodles_ch1/SED_004.dat"
 
-    wavelength_dependence(
-        time_scale=1.0, psf_filename=psf_filename, target_filename=target_filename
+    wavelength_dependence_airs(
+        time_scale=1.0,
+        psf_filename=psf_filename,
+        target_filename=target_filename,
     )
 
     plt.show()
--- pyxel/models/scene_generation/ariel_airs_calculation.py
+++ pyxel/models/scene_generation/ariel_airs_calculation.py
@@ -1,31 +1,41 @@
 """Pyxel photon generator models."""
 import os
-from typing import Literal, Optional, Sequence, Tuple
+from typing import Tuple
 
 import numpy as np
 import pandas as pd
-import scipy.constants as cst
+
+# import scipy.constants as cst
 from astropy import units as u
 from astropy.io import ascii, fits
+from astropy.units import Quantity
+from numpy import ndarray
 from scipy.integrate import cumtrapz
 
 
 # ---------------------------------------------------------------------------------------------
-def read_star_flux_from_file(filename):
+def read_star_flux_from_file(
+    filename: str,
+    verbose: bool = False,
+) -> tuple[np.ndarray, np.ndarray]:
     """
     Read star flux file.
-    TODO: Read the unit from the text file
+    # TODO: Read the unit from the text file.
 
-    :param:
-        filename type string,
-                name of the target file. Different extension can be considered
-    :return:
-        wavelength: type array 1D
-                Wavelength associated with the flux of the star
-        flux: type array 1D
-                Flux of the target considered, in  ph/s/m2/µm
+    Parameters
+    ----------
+    filename: type string,
+        Name of the target file. Different extension can be considered.
+    verbose : bool
+        If True information is displayed. Default False.
+
+    Returns
+    -------
+    wavelength: type array 1D
+        Wavelength associated with the flux of the star.
+    flux: type array 1D
+        Flux of the target considered, in  ph/s/m2/µm.
     """
-    BOOL_verbose = True
     extension = os.path.splitext(filename)[1]
     if extension == ".txt":
         wavelength, flux = np.loadtxt(filename).T
@@ -54,61 +64,77 @@ def read_star_flux_from_file(filename):
         wavelength = wavelength * u.micron
         flux = flux * u.photon / u.s / u.m / u.m / u.micron
     else:
-        message("ERROR while convering, extension not readable", BOOL_verbose)
+        print("ERROR while converting, extension not readable", verbose)
 
     return wavelength, flux
 
 
 # ---------------------------------------------------------------------------------------------
-def convert_flux(wavelength, flux, telescope_diameter_m1, telescope_diameter_m2):
+def convert_flux(
+    wavelength: np.ndarray,
+    flux: np.ndarray,
+    telescope_diameter_m1: float,
+    telescope_diameter_m2: float,
+    verbose: bool = False,
+) -> np.ndarray:
     """
-    Convert the flux of the target in ph/s/µm
+    Convert the flux of the target in ph/s/µm.
 
     Parameters
     ----------
     wavelength: 1D array
-        Wavelength sampling of the considered target
+        Wavelength sampling of the considered target.
     flux: 1D array
-        Flux of the target considered in ph/s/m2/µm
+        Flux of the target considered in ph/s/m2/µm.
     telescope_diameter_m1: float
-        Diamater of the M1 mirror of the TA
+        Diameter of the M1 mirror of the TA.
     telescope_diameter_m2: float
-        Diamater of the M2 mirror of the TA
+        Diameter of the M2 mirror of the TA.
+    verbose : bool
+        If True information is displayed. Default False.
 
     Returns
     --------
     conv_flux: 1D array
-        Flux of the target considered in ph/s/µm
-
+        Flux of the target considered in ph/s/µm.
     """
-    BOOL_verbose = False
-    if BOOL_verbose:
-        print("Incident photon flux is being converted into ph/s/um")
+    if verbose:
+        print("Incident photon flux is being converted into ph/s/um.")
     # use of astropy code
     flux.to(
         u.photon / u.m**2 / u.micron / u.s,
         equivalencies=u.spectral_density(wavelength),
     )
 
-    diameter = (
+    diameter_m2 = (
         telescope_diameter_m2 * u.meter
     )  # TODO: define a class to describe the optic of the telescope ?
-    Diameter = telescope_diameter_m1 * u.meter
-    collecting_area = np.pi * diameter * Diameter / 4
+    diameter_m1 = telescope_diameter_m1 * u.meter
+    collecting_area = np.pi * diameter_m1 * diameter_m2 / 4
     conv_flux = np.copy(flux) * collecting_area
     return conv_flux
 
 
 # ---------------------------------------------------------------------------------------------
-def compute_bandwidth(psf_wavelength):
+def compute_bandwidth(
+    psf_wavelength,
+) -> tuple[Quantity, Quantity]:
     """
-    Computes the bandwidth for non even distributed values
+    Compute the bandwidth for non even distributed values.
     First we put the poles, each pole is at the center of the previous wave and the next wave.
     We add the first pole and the last pole using symmetry. We get nw+1 poles
 
-    :param psf:   PSF object
-    :return:        bandwidth (usually in microns) and the pole wavelengths
-    :rtype:         type quantity array,dimension nw
+    Parameters
+    ----------
+    psf_wavelength:
+       PSF object.
+
+    Returns
+    -------
+    bandwidth : quantity array
+        Bandwidth. Usually in microns.
+    all_poles : quantity array
+        Pole wavelengths. Dimension: nw
     """
     poles = (psf_wavelength[1:] + psf_wavelength[:-1]) / 2
     first_pole = psf_wavelength[0] - (psf_wavelength[1] - psf_wavelength[0]) / 2
@@ -120,32 +146,50 @@ def compute_bandwidth(psf_wavelength):
 
 
 # ---------------------------------------------------------------------------------------------
-def integrate_flux(wavelength, flux, psf_wavelength):
+def integrate_flux(
+    wavelength: Quantity,
+    flux: Quantity,
+    psf_wavelength: Quantity,
+    verbose: bool = False,
+) -> Quantity:
     """
-    Integrate flux on each bin around the psf.
-    The trick is to integrate first, and interpolate after (and not vice-versa).
+     Integrate flux on each bin around the psf.
+     The trick is to integrate first, and interpolate after (and not vice-versa).
 
-    :param flux:        type quantity array, unit ph/s/m2/micron dimension big_n
-    :param  wavelength:   type quantity array, unit usually micron, dimension small_n
+     Parameters
+     ----------
+     wavelength : quantity array
+         Wavelength. Unit: usually micron. Dimension: small_n.
+     flux : quantity array
+         Flux. Unit: ph/s/m2/micron. Dimension: big_n.
+     psf_wavelength : quantity array
+         Point Spead Function per wavelength.
+    verbose : bool
+         If True information is displayed. Default False.
 
-    :return: flux integrated in photon/s
-    :rtype: type quantity array, dimension nw
+     Returns
+     -------
+     flux : quantity array
+         Flux. UNit: photon/s. Dimension: nw.
     """
-    BOOL_verbose = True
-    if BOOL_verbose:
+    if verbose:
         print("Integrate flux on each bin around the psf...")
 
     bandwidth, all_poles = compute_bandwidth(
         psf_wavelength
-    )  # Mettre les paramètres de la fonction
+    )  # Set the parameters of the function
+
     cum_sum = cumtrapz(flux.value, wavelength.value, initial=0.0)  # Cumulative count
+
     # self.wavelength has to quantity: value and units
     cum_sum_interp = np.interp(
         all_poles, wavelength, cum_sum
     )  # interpolate over psf wavelength
+
     flux_int = (
         cum_sum_interp[1:] - cum_sum_interp[:-1]
     )  # Compute flux over psf spectral bin
+
     flux_int = flux_int * flux.unit * wavelength.unit
     flux = np.copy(flux_int)  # Update flux matrix
 
@@ -153,35 +197,50 @@ def integrate_flux(wavelength, flux, psf_wavelength):
 
 
 # ------------------------------------------------------------------------------
-def multiply_by_transmission(psf, transmission_dict):
-    """The goal of this function is to take into account the flux of the incident star"""
-    for t in transmission_dict.keys():
-        if "M" in t:
-            f = interpolate.interp1d(
-                transmission_dict[t]["wavelength"],
-                transmission_dict[t]["reflectivity_eol"],
-            )
-        else:
-            f = interpolate.interp1d(
-                transmission_dict[t]["wavelength"],
-                transmission_dict[t]["transmission_eol"],
-            )
-        flux = np.copy(flux) * f(psf.psf_wavelength)
+# def multiply_by_transmission(psf, transmission_dict: Mapping[str, Any]) -> None:
+#     """The goal of this function is to take into account the flux of the incident star"""
+#     for t in transmission_dict.keys():
+#         if "M" in t:
+#             f = interpolate.interp1d(
+#                 transmission_dict[t]["wavelength"],
+#                 transmission_dict[t]["reflectivity_eol"],
+#             )
+#         else:
+#             f = interpolate.interp1d(
+#                 transmission_dict[t]["wavelength"],
+#                 transmission_dict[t]["transmission_eol"],
+#             )
+#         flux = np.copy(flux) * f(psf.psf_wavelength)
+#
 
 
 # ---------------------------------------------------------------------------------------------
-def read_psf_from_fits_file(filename):
+def read_psf_from_fits_file(
+    filename: str,
+    verbose: bool = False,
+) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """
-    Read psf files depending on simulation and instrument parameters
+    Read psf files depending on simulation and instrument parameters.
 
-    :param:     filename : type string, name of the .fits file
-                verbose : type bool, if True information are displayed. Default False
+    Parameters
+    ----------
+    filename : str
+        Name of the .fits file.
+    verbose : bool
+        If True information is displayed. Default False.
 
-    :return:    cube : (3D array) PSF for each wavelength saved as (360,360) array, one for each wavelengh
-                waves (1D array) wavelength in um
-                line_pos_psf, col_pos_psf : 1D array, position of the PSF at each wavelength
+    Returns
+    -------
+    psf_datacube : ndarray
+        3D array, PSF for each wavelength saved as array, one for each wavelength
+        waves (1D array) wavelength. Unit: um.
+    psf_wavelength : ndarray
+        1D array, wavelengths. Unit: um.
+    line_pos_psf : ndarray
+        1D array, x position of the PSF at each wavelength.
+    col_pos_psf : ndarray
+        1D array, y position of the PSF at each wavelength.
     """
-    BOOL_verbose = True
     hdu = fits.open(filename)  # Open fits
     psf_datacube, table = hdu[0].data, hdu[1].data
     line_psf_pos = (table["x_centers"]).astype(
@@ -192,7 +251,7 @@ def read_psf_from_fits_file(filename):
     )  # Position of the PSF on AIRS window along col
     psf_wavelength = table["waves"] * u.micron  # Wavelength
     hdu.close()  # Close fits
-    if BOOL_verbose:
+    if verbose:
         print(
             "PSF Datacube", psf_datacube.shape, psf_datacube.min(), psf_datacube.max()
         )
@@ -208,8 +267,14 @@ def read_psf_from_fits_file(filename):
 
 # ---------------------------------------------------------------------------------------------
 def project_psfs(
-    psf_datacube, line_psf_pos, col_psf_pos, flux, row, col, expend_factor
-):
+    psf_datacube: np.ndarray,
+    line_psf_pos: Quantity,
+    col_psf_pos,
+    flux,
+    row,
+    col,
+    expend_factor: float,
+) -> tuple[ndarray, ndarray]:
     """
     Project each psf on a (n_line_final * self.zoom, n_col_final * self.zoom) pixel image
     n_line_final, n_col_final = corresponds to window size. It varies with the channel
@@ -278,34 +343,50 @@ def project_psfs(
 
 
 # ---------------------------------------------------------------------------------------------
-def rebin_2d(data, expend_factor):
+def rebin_2d(
+    data: np.ndarray,
+    expand_factor: float,
+    verbose: bool = False,
+) -> np.ndarray:
     """
-    rebin as idl
+    Rebin as idl.
     Each pixel of the returned image is the sum of zy by zx pixels of the input image.
 
-    Parameters :   data: numpy.array of 2 dimensions (image), ny, nx
-                   expend_factor: tuple of 2 integers, zy, zx
-                   beware ny must be a multiple of zy, and nx multiple of zx
-    verbose: bool, displays the final shape
-
-    Return :       result: numpy array shrinked, dimension ny/zy, nx/zx
-
-    Example :      a = np.arange(48).reshape((6,8))
-                   rebin2d( a, [2,2])
-
-    HISTORY:       Rene Gastaud, 13 January 2016
+    Based on:       Rene Gastaud, 13 January 2016
     https://codedump.io/share/P3pB13TPwDI3/1/resize-with-averaging-or-rebin-a-numpy-2d-array
     2017-11-17 : RG and Alan O'Brien  compatibility with python 3 bug452
+
+    Parameters
+    ----------
+    data : ndarray
+        Data with 2 dimensions (image): ny, nx.
+    expand_factor : tuple
+        Expansion factor is a tuple of 2 integers: zy, zx.
+    verbose : bool
+        If True information is displayed. Default False.
+
+    Returns
+    -------
+    result : ndarray
+        Shrunk in dimension ny/zy, nx/zx.
+
+
+    Example
+    -------
+    a = np.arange(48).reshape((6,8))
+               rebin2d( a, [2,2])
+
+
     """
-    BOOL_verbose = True
-    zoom = [expend_factor, expend_factor]  # In case assymetrical zoom is used
+
+    zoom = [expand_factor, expand_factor]  # In case asymmetrical zoom is used
     final_shape = (
         int(data.shape[0] // zoom[0]),
         zoom[0],
         int(data.shape[1] // zoom[1]),
         zoom[1],
     )
-    if BOOL_verbose:
+    if verbose:
         print("final_shape ", final_shape)
     result = data.reshape(final_shape).sum(3).sum(1)
     return result
