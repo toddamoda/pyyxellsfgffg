2da80018ff38dd6b6bdbf48449db518510f12bc0
frederic lemmel <frederic.lemmel@ext.esa.int>
2023-04-05 10:07:20 +0200

    Bug fixing.

--- pyxel/models/charge_generation/apd_gain.py
+++ pyxel/models/charge_generation/apd_gain.py
@@ -4,22 +4,7 @@
 #  is part of this Pyxel package. No part of the package, including
 #  this file, may be copied, modified, propagated, or distributed except according to
 #  the terms contained in the file ‘LICENCE.txt’.
-#
-#
-#
-#  This file is subject to the terms and conditions defined in file 'LICENCE.txt', which
-#  is part of this Pyxel package. No part of the package, including
-#  this file, may be copied, modified, propagated, or distributed except according to
-#  the terms contained in the file ‘LICENCE.txt’.
-#
-#
-#
-#  This file is subject to the terms and conditions defined in file 'LICENCE.txt', which
-#  is part of this Pyxel package. No part of the package, including
-#  this file, may be copied, modified, propagated, or distributed except according to
-#  the terms contained in the file ‘LICENCE.txt’.
-#
-#
+
 """Avalanche gain model."""
 from pyxel.detectors import APD
 
