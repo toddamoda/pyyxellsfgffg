07c1382c11ae2e0ac58ae0e0da00de3c912bfca8
constanze.seibert <constanze.seibert@esa.int>
2023-04-17 13:14:05 +0200

    Applied changes from mypy and flake8.

--- pyxel/models/data_processing/statistics.py
+++ pyxel/models/data_processing/statistics.py
@@ -7,7 +7,7 @@
 
 """Simple model to compute basic statistics."""
 from collections.abc import Sequence
-from typing import Literal, Optional, Union
+from typing import Literal, Union
 
 import xarray as xr
 from datatree import DataTree
@@ -18,17 +18,23 @@ from pyxel.detectors import Detector
 
 def compute_statistics(
     data_array: xr.DataArray,
+    absolute_time: xr.DataArray,
     dimensions: Union[str, Sequence[str]] = ("x", "y"),
-) -> xr.DataArray:
-    """Compute basic statistics.= into a Dataarray.
+) -> xr.Dataset:
+    """Compute basic statistics and save it into a Dataset.
 
     Parameters
     ----------
     data_array : DataArray
+        Input array which contains the data.
+    absolute_time : DataArray
+        Absolute time(s) of the data.
+    dimensions : str or Sequence of str
+        Dimensions, where the statistics should be computed on. Default is ("x", "y").
 
     Returns
     -------
-    DataArray
+    Dataset
     """
 
     var = data_array.var(dim=dimensions)
@@ -37,27 +43,21 @@ def compute_statistics(
     max_array = data_array.max(dim=dimensions)
     count = data_array.count(dim=dimensions)
 
-    statistics: xr.DataArray = count.expand_dims(
-        var=[var], mean=[mean], min_array=[min_array], max_array=[max_array]
-    )  # ,count=[count])
-    #
-    # dataset = xr.Dataset().assign_coords(time=absolute_time)
-    # dataset["var"] = var
-    # dataset["mean"] = mean
-    # dataset["min"] = min_array
-    # dataset["max"] = max_array
-    # dataset["count"] = count
+    dataset = xr.Dataset()
+    dataset["var"] = var
+    dataset["mean"] = mean
+    dataset["min"] = min_array
+    dataset["max"] = max_array
+    dataset["count"] = count
 
-    return statistics
+    dataset = dataset.expand_dims(dim="time").assign_coords(time=absolute_time)
 
-    # detector.processed_data.append(dataset)
-    # detector.data["/statistics"] = DataTree(dataset)
+    return dataset
 
 
 def statistics(
     detector: Detector,
-    data_structure: Literal["pixel", "photon", "image", "signal"] = "pixel",
-    name: Optional[str] = None,
+    data_structure: Literal["pixel", "photon", "signal", "image", "all"] = "all",
 ) -> None:
     """Compute basic statistics.
 
@@ -67,40 +67,55 @@ def statistics(
         Pyxel Detector object.
     data_structure : Literal
         Keyword to choose data structure. Can be any from:
-        ("pixel", "photon", "image", "signal")
-    dimensions : str or Sequence of str
-        Dimensions.
+        ("pixel", "photon", "signal","image", "all").
+        Default is "all" and computes the statistics on "pixel", "photon", "signal" and "image".
+
     """
-    if name is None:
-        name = data_structure
-
-    # Extract data from 'detector'
-    data_bucket: Union[Pixel, Photon, Image, Signal] = getattr(detector, data_structure)
-    data_array: xr.DataArray = data_bucket.to_xarray()
-
-    # Get statistics data
-    statistics: xr.DataArray = compute_statistics(data_array)
-
-    parent: str = "/statistics"
-    parent_partial: str = f"{parent}/partial"
-    key: str = f"{parent}/{name}"
-    key_partial: str = f"{parent_partial}/{name}"
-
-    try:
-        _ = detector.data[key_partial]
-    except KeyError:
-        has_key_partial = False
+    if data_structure == "all":
+        names = ["pixel", "photon", "signal", "image"]
     else:
-        has_key_partial = True
+        names = [data_structure]
 
-    if not has_key_partial:
-        data_tree: DataTree = DataTree(statistics)
-    else:
-        # Concatenate data
-        data_tree = detector.data[key_partial].combine_first(statistics)  # type: ignore
+    for name in names:
+        # Extract data from 'detector'
+        data_bucket: Union[Pixel, Photon, Signal, Image] = getattr(detector, name)
+        data_array: xr.DataArray = data_bucket.to_xarray()
+
+        # Get current absolute time
+        absolute_time = xr.DataArray(
+            [detector.absolute_time],
+            dims=["time"],
+            attrs={"units": "s"},
+        )
+
+        # Get statistics data
+        statistics: xr.Dataset = compute_statistics(
+            data_array=data_array, absolute_time=absolute_time
+        )
+
+        parent: str = "/statistics"
+        parent_partial: str = f"{parent}/partial"
+        key: str = f"{parent}/{name}"
+        key_partial: str = f"{parent_partial}/{name}"
+
+        try:
+            _ = detector.data[key_partial]
+        except KeyError:
+            has_key_partial = False
+        else:
+            has_key_partial = True
+
+        if not has_key_partial:
+            data_tree: DataTree = DataTree(statistics)
+        else:
+            # Concatenate data
+            previous_datatree = detector.data[key_partial]
+            data_tree = previous_datatree.combine_first(statistics)  # type: ignore
+
+        if detector.pipeline_count == (detector.num_steps - 1):
+            detector.data[key] = data_tree
+        else:
+            detector.data[key_partial] = data_tree
 
     if detector.pipeline_count == (detector.num_steps - 1):
         detector.data[parent_partial].orphan()
-        detector.data[key] = data_tree.sortby("mean")
-    else:
-        detector.data[key_partial] = data_tree
