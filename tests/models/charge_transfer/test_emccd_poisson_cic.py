ed21cb3d22dc50a65b6998d841c13f53ec0d6c18
Arthur Kadela <cpv181@alumni.ku.dk>
2023-05-30 18:41:58 +0000

    Update file test_emccd_poisson_cic.py

--- tests/models/charge_transfer/test_emccd_poisson_cic.py
+++ tests/models/charge_transfer/test_emccd_poisson_cic.py
@@ -7,21 +7,21 @@ from pyxel.models.charge_transfer.EMCCD_poisson_cic import multiplication_regist
 def test_multiplication_register(ccd_10x10: CCD):
     detector = ccd_10x10
 
-    multiplication_register(detector=detector, total_gain=0.0, gain_elements=1, pCIC_rate=0.0, sCIC_rate=0.0)
+    multiplication_register(detector=detector, total_gain=0.0, gain_elements=1, pcic_rate=0.0, scic_rate=0.0)
 
 
 @pytest.mark.parametrize(
-    "total_gain,gain_elements,pCIC_rate,sCIC_rate",
+    "total_gain,gain_elements,pcic_rate,scic_rate",
     [
         (-1, 10, 0.0, 0.0),
         (1, -1, -1, 1),
         (-1, -1, .005, -0.02),
     ],
 )
-def test_multiplication_register_bad_inputs(ccd_10x10: CCD, total_gain, gain_elements, pCIC_rate, sCIC_rate):
+def test_multiplication_register_bad_inputs(ccd_10x10: CCD, total_gain, gain_elements, pcic_rate, scic_rate):
     detector = ccd_10x10
 
     with pytest.raises(ValueError, match="Wrong input parameter"):
         multiplication_register(
-            detector=detector, total_gain=total_gain, gain_elements=gain_elements, pCIC_rate=pCIC_rate, sCIC_rate=sCIC_rate
+            detector=detector, total_gain=total_gain, gain_elements=gain_elements, pcic_rate=pcic_rate, scic_rate=scic_rate
         )
