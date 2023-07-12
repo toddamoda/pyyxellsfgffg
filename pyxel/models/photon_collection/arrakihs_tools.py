d8ab813a8f5c98176250912136f807e75c08ce68
constanze.seibert <constanze.seibert@esa.int>
2023-07-12 11:22:32 +0200

    Correct typo.

--- pyxel/models/photon_collection/arrakihs_tools.py
+++ pyxel/models/photon_collection/arrakihs_tools.py
@@ -133,8 +133,8 @@ def detector_coordinates(
 
 
 def num_photon_sky(
-    wvs_all_sorted: np.ndarray, nphoton_all_sorted: np.ndarray, x: np.ndarray
-) -> np.ndarray:
+    wvs_all_sorted: np.array, nphoton_all_sorted: np.array, x: np.array
+) -> np.array:
     """Interpolate the photons from the spectrum and the wave lengths.
 
     Parameters
