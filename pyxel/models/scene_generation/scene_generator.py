70a0a308ee1d72cd31875be758b93905264815d0
constanze.seibert <constanze.seibert@esa.int>
2023-07-25 14:32:56 +0200

    Update astroquery.

--- pyxel/models/scene_generation/scene_generator.py
+++ pyxel/models/scene_generation/scene_generator.py
@@ -7,10 +7,10 @@
 import astropy
 import astropy.units as u
 import scopesim
-import scopesim_templates
 from astropy.coordinates import SkyCoord
 from astropy.io import fits
 from astroquery.gaia import Gaia
+from scopesim_templates import stars
 from scopesim_templates.rc import Source
 from scopesim_templates.utils import general_utils as gu
 
@@ -20,21 +20,14 @@ from pyxel.detectors import Detector
 def load_objects_from_gaia(
     right_ascension: float,
     declination: float,
-    fov_width: float,
-    fov_heigth: float,
+    fov_radius: float,
 ) -> astropy.table.table.Table:
     """Load objects from GAIA Catalog for given coordinactes and FOV.
 
     Parameters
     ----------
-    right_ascension : float
-        RA coordinates of object.
-    declination : float
-        DEC coordinates of object.
-    fov_width : float
-        Width of FOV of optics.
-    fov_heigth : float
-        Height of FOV of optics.
+    coords_detector: SkyCoord (ICRS): (ra, dec) in deg.
+            Coordinates of the pointing of the telescope.
 
     Returns
     -------
@@ -44,49 +37,54 @@ def load_objects_from_gaia(
     # The coordinates of the object
     # Frame: Type of coordinate frame this SkyCoord should represent.
     # Default now is International Celestial Reference System (ICRS).
-    coord = SkyCoord(
-        ra=right_ascension,
-        dec=declination,
-        frame="icrs",
-    )
+    # coord = SkyCoord(
+    #     ra=right_ascension,
+    #     dec=declination,
+    #     frame="icrs",
+    # )
     # alternative to give over ra and dec and calculate the sycoords one could directly use skycoords as input like alex
     # coords_detector: SkyCoord (ICRS): (ra, dec) in deg. # Coordinates of the pointing of the telescope.
 
     # The FOV of the optics limits the region in the sky together with the coordinates of the pointing of the optics.
     # alternative: fov_radius, if we assume its always round
-    width = fov_width * u.arcsec
-    height = fov_heigth * u.arcsec
+    radius = fov_radius * u.arcsec
 
     # Unlimited rows.
     Gaia.ROW_LIMIT = -1
-    # we get the data from GAIA DR3 at the coordinates
+    # we get the data from GAIA DR3
     Gaia.MAIN_GAIA_TABLE = "gaiadr3.gaia_source"
 
     # Query for the catalog to search area with coordinates in FOV of optics.
-    objects = Gaia.query_object_async(
-        coordinate=coord,
-        width=width,
-        height=height,
+    job = Gaia.launch_job_async(
+        f"SELECT source_id, ra, dec, has_xp_sampled, phot_bp_mean_mag, phot_g_mean_mag, phot_rp_mean_mag \
+    FROM gaiadr3.gaia_source \
+    WHERE CONTAINS(POINT('ICRS', ra, dec),CIRCLE('ICRS',{right_ascension},{declination},{radius}))=1 \
+    AND has_xp_sampled = 'True'"
     )
+
+    query = job.get_results()
+    spectrum_list = Gaia.load_data(
+        ids=query["source_id"], retrieval_type="XP_SAMPLED", data_release="Gaia DR3"
+    )
+
+    # objects = Gaia.query_object_async(
+    #     coordinate=coords_detector,
+    #     radius=radius
+    # )
 
     return objects
 
 
 def generate_scene(
     detector: Detector,
-    right_ascension: float,
-    declination: float,
-    fov_width: float,
-    fov_heigth: float,
+    coords_detector: SkyCoord,
+    fov_radius: float,
     filter_name: str = "H",  # or list?
     spectrum_type: str = "A0V",  # or list?
     # magnitude:
 ):
     objects = load_objects_from_gaia(
-        right_ascension=right_ascension,
-        declination=declination,
-        fov_width=fov_width,
-        fov_heigth=fov_heigth,
+        coords_detector=coords_detector, fov_radius=fov_radius
     )
 
     # Convert the positions to arcseconds and center around 0.0, 0.0.
@@ -96,7 +94,7 @@ def generate_scene(
     # Select a nice magnitude of the sources.
     mags = objects["phot_g_mean_mag"]  # could be an input as well
 
-    scene = scopesim_templates.stellar.stars(
+    scene = stars(
         filter_name=filter_name,
         spec_types=spectrum_type,
         amplitudes=mags,
