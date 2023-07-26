d512b2c73f7839a38410eb1f8e3cf761b05f5908
frederic lemmel <frederic.lemmel@ext.esa.int>
2023-07-26 13:49:07 +0200

    Fix issue in unit tests.

--- tests/models/data_processing/test_linear_regression.py
+++ tests/models/data_processing/test_linear_regression.py
@@ -5,18 +5,14 @@
 #   this file, may be copied, modified, propagated, or distributed except according to
 #   the terms contained in the file ‘LICENCE.txt’.
 
+from typing import Tuple
+
 import numpy as np
 import pytest
-import statsmodels.api as s
+import statsmodels.api as sm
 import xarray as xr
 
-from pyxel.detectors import (
-    CCD,
-    CCDGeometry,
-    Characteristics,
-    Environment,
-    ReadoutProperties,
-)
+from pyxel.detectors import CCD, CCDGeometry, Characteristics, Environment
 from pyxel.models.data_processing import linear_regression
 
 
@@ -34,7 +30,7 @@ def ccd_10x10() -> CCD:
         environment=Environment(),
         characteristics=Characteristics(),
     )
-    detector.set_readout(num_steps=3, start_time=1.0, end_time=4.0)
+    detector.set_readout(num_steps=3, start_time=0.0, end_time=4.0)
 
     return detector
 
@@ -53,75 +49,82 @@ def test_raise_error():
         environment=Environment(),
         characteristics=Characteristics(),
     )
-    detector.set_readout(num_steps=2, start_time=1.0, end_time=4.0)
+    detector.set_readout(num_steps=2, start_time=0.0, end_time=4.0)
 
     with pytest.raises(ValueError, match="Expecting at least 3 steps"):
         linear_regression(detector=detector)
 
 
 @pytest.fixture
-def image_3d():
+def image_3d() -> xr.DataArray:
     """Create fake image for three different times."""
 
     rng = np.random.default_rng(seed=100)
-    absolute_time = [1.0, 2.0, 3.0]
+    relative_time = [1.0, 2.0, 3.0]
     image = xr.DataArray(
         rng.random((3, 10, 10)),
         dims=["time", "y", "x"],
-        coords={"time": absolute_time, "y": range(10), "x": range(10)},
+        coords={"time": relative_time, "y": range(10), "x": range(10)},
     )
     return image
 
 
 @pytest.fixture
-def expected_result(image_3d):
-    """Create expected result."""
-    computed_result = image_3d.polyfit(dim="time", deg=1, full=True, cov=True)
+def expected_result(image_3d: xr.DataArray) -> xr.Dataset:
+    """Compute expected result with 'statsmodels'."""
 
-    # create expected Dataset
-    expected = xr.Dataset()
-    expected["slope"] = computed_result["polyfit_coefficients"].isel(degree=0)
-    expected["intercept"] = computed_result["polyfit_coefficients"].isel(degree=1)
-    # expected["r2"]=computed_result["polyfit_residuals"]
-    expected["slope_std"] = np.sqrt(
-        computed_result["polyfit_covariance"].sel(cov_i=0, cov_j=0)
-    )
-    expected["intercept_std"] = np.sqrt(
-        computed_result["polyfit_covariance"].sel(cov_i=1, cov_j=1)
-    )
-    del expected["degree"]
+    def compute_linear_regression(
+        y: np.ndarray, x: np.ndarray
+    ) -> tuple[float, float, float, float, float]:
+        model = sm.OLS(endog=y, exog=sm.add_constant(x))
+        results = model.fit()
 
-    return expected
+        intercept, slope = results.params
+        r2 = results.rsquared
+        intercept_std, slope_std = results.bse
+
+        return intercept, slope, r2, intercept_std, slope_std
+
+    all_results = xr.apply_ufunc(
+        compute_linear_regression,
+        image_3d,
+        image_3d["time"],
+        input_core_dims=[["time"], ["time"]],
+        output_core_dims=[[], [], [], [], []],
+        vectorize=True,
+    )
+    intercept, slope, r2, intercept_std, slope_std = all_results
+
+    ds = xr.Dataset()
+    ds["intercept"] = intercept
+    ds["slope"] = slope
+    ds["r2"] = r2
+    ds["intercept_std"] = intercept_std
+    ds["slope_std"] = slope_std
+
+    return ds
 
 
 def test_linear_regression(
-    ccd_10x10: CCD, image_3d: np.ndarray, expected_result: xr.Dataset
+    ccd_10x10: CCD,
+    image_3d: xr.DataArray,
+    expected_result: xr.Dataset,
 ):
     """Test model 'linear_regression'."""
 
     detector = ccd_10x10
-    image = image_3d
     steps = [1.0, 1.0, 1.0]
     absolute_time = image_3d.time.to_numpy()
-
-    model = s.OLS(
-        endog=image_3d.isel(x=0, y=0).to_numpy(), exog=s.add_constant(absolute_time)
-    )
-    results = model.fit()
 
     for i, (time, step) in enumerate(zip(absolute_time, steps)):
         detector.time = time
         detector.time_step = step
         detector.pipeline_count = i
 
-        detector.image.array = image.isel(time=i).to_numpy()
+        detector.image.array = image_3d.isel(time=i).to_numpy()
         linear_regression(detector=detector, data_structure="image")
 
-    data_array = detector.data["/linear_regression/image"]
-    # convert to Dataset
-    data_array = data_array.to_dataset()
-    # TODO: Expand test for r2 variable.
-    data_array = data_array.drop_vars("r2")
+    dataset = detector.data["/linear_regression/image"].to_dataset()
 
     # check that both datasets are the same
-    xr.testing.assert_allclose(data_array, expected_result)
+    xr.testing.assert_allclose(dataset, expected_result)
