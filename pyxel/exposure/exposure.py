47a9befbc4972f1cc3cdcdd0efd105480474b654
Frederic Lemmel <frederic.lemmel@ext.esa.int>
2023-11-15 18:08:52 +0100

    Fix issue when '._array' property for 'Photon', 'Pixel', 'Image', 'Signal' or 'Charge' is None.

--- pyxel/exposure/exposure.py
+++ pyxel/exposure/exposure.py
@@ -9,8 +9,8 @@
 """Observation class and functions."""
 
 import logging
-import operator
-from collections.abc import Mapping, Sequence
+from collections import defaultdict
+from collections.abc import Sequence
 from typing import TYPE_CHECKING, Optional, Union
 
 import numpy as np
@@ -19,7 +19,7 @@ from datatree import DataTree
 from tqdm.auto import tqdm
 
 from pyxel import __version__
-from pyxel.data_structure import Charge, Image, Photon, Pixel, Signal
+from pyxel.data_structure import Charge, Image, Photon, Pixel, Scene, Signal
 from pyxel.pipelines import Processor, ResultId, get_result_id, result_keys
 from pyxel.util import set_random_seed
 
@@ -68,6 +68,7 @@ class Exposure:
         """TBW."""
         self._pipeline_seed = value
 
+    # TODO: This function will be deprecated
     def run_exposure(self, processor: Processor) -> "xr.Dataset":
         """Run an observation pipeline.
 
@@ -192,8 +193,7 @@ def run_exposure_pipeline(
 
         keys = result_keys(result_type)
 
-        unstacked_result: Mapping[str, list] = {key: [] for key in keys}
-
+        unstacked_result: dict[str, list] = defaultdict(list)
         i: int
         time: float
         step: float
@@ -219,19 +219,31 @@ def run_exposure_pipeline(
                 outputs.save_to_file(processor)
 
             for key in keys:
-                if key == "data":
+                if key in ("data", "scene"):
                     continue
 
-                unstacked_result[key].append(
-                    np.array(operator.attrgetter(key)(detector))
+                obj: Union[Scene, Photon, Pixel, Image, Signal, Charge] = getattr(
+                    detector, key
                 )
+
+                # TODO: Is this necessary ?
+                if not isinstance(obj, (Photon, Pixel, Image, Signal, Charge)):
+                    raise TypeError(
+                        f"Wrong type from attribute 'detector.{key}'. Type: {type(obj)!r}"
+                    )
+
+                if obj._array is not None:
+                    data_arr: np.ndarray = np.array(obj)
+                    unstacked_result[key].append(data_arr)
 
             if progressbar:
                 pbar.update(1)
 
         # TODO: Refactor '.result'. See #524
         processor.result = {
-            key: np.stack(unstacked_result[key]) for key in keys if key != "data"
+            key: np.stack(value)
+            for key, value in unstacked_result.items()
+            if key != "data"
         }
 
         if progressbar:
@@ -292,6 +304,7 @@ def _extract_datatree(detector: "Detector", keys: Sequence[ResultId]) -> DataTre
 
         obj: Union[Photon, Pixel, Image, Signal, Charge] = getattr(detector, key)
 
+        # TODO: Is this necessary ?
         if not isinstance(obj, (Photon, Pixel, Image, Signal, Charge)):
             raise TypeError(
                 f"Wrong type from attribute 'detector.{key}'. Type: {type(obj)!r}"
