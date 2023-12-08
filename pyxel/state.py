ff2fd18f3a36b60d452c5ee54bb8e96d8275fa11
Frederic Lemmel <frederic.lemmel@ext.esa.int>
2023-12-08 10:34:30 +0100

    Remove unused functions 'get_obj_by_type', 'get_state_ids' and 'copy_state'.

--- pyxel/state.py
+++ pyxel/state.py
@@ -6,28 +6,12 @@
 #  the terms contained in the file ‘LICENCE.txt’.
 
 """TBW."""
-from collections.abc import Mapping, Sequence
 from typing import Any, Optional
 
-__all__ = ["get_obj_att", "get_obj_by_type", "get_state_ids", "copy_state"]
+__all__ = ["get_obj_att"]
 
 
-# TODO: Remove this function ? See issue #230.
-def get_obj_by_type(obj: Any, key: str, obj_type: Optional[type] = None) -> Any:
-    """Get the object associated with the class type following the key chain.
-
-    :param obj:
-    :param key:
-    :param obj_type:
-    :return:
-    """
-    obj, att = get_obj_att(obj, key, obj_type)
-    if obj_type is not None:
-        if isinstance(obj, obj_type):
-            return obj
-
-
-# TODO: Remove this function ? See issue #230.
+# TODO: Refactor this function ?
 def get_obj_att(obj: Any, key: str, obj_type: Optional[type] = None) -> tuple[Any, str]:
     """Get the object associated with the key.
 
@@ -78,92 +62,3 @@ def get_obj_att(obj: Any, key: str, obj_type: Optional[type] = None) -> tuple[An
             obj = None
             break
     return obj, tail
-
-
-# TODO: Remove this function ? See issue #230.
-def get_state(obj: Any) -> Mapping[str, Any]:
-    """Convert the config object to a embedded dict object.
-
-    The returned value will be a dictionary tree that is JSON
-    compatible. Only dict, list, str, numbers will be contained
-    in the returned dictionary.
-
-    :param obj: a top level dict object or a object that defines
-        the __getstate__ method.
-
-    :return: the dictionary representation of the passed object.
-    """
-    result = {}
-    if isinstance(obj, dict):
-        for key, value in obj.items():
-            result[key] = value.__getstate__()
-
-    elif hasattr(obj, "__getstate__"):
-        result = obj.__getstate__()
-
-    return result
-
-
-# TODO: Remove this function ? See issue #230.
-def get_state_ids(
-    obj: Any,
-    parent_key_list: Optional[Sequence[str]] = None,
-    result: Optional[dict[str, Any]] = None,
-) -> Any:
-    """Retrieve a flat dictionary of the object attribute hierarchy.
-
-    The dot-format is used as the key representation.
-
-    :param obj:
-    :param parent_key_list:
-    :param result:
-    :return:
-    """
-    if result is None:
-        from collections import OrderedDict
-
-        result = OrderedDict()
-
-    if parent_key_list is None:
-        parent_key_list = []
-
-    if isinstance(obj, dict):
-        for key, value in obj.items():
-            if isinstance(value, (str, int, float)):
-                key = ".".join(parent_key_list) + "." + key
-                result[key] = value
-            else:
-                _ = [*parent_key_list, key]  # TODO: Is it used ??
-                get_state_ids(value, [*parent_key_list, key], result)
-
-    elif isinstance(obj, list):
-        is_primitive = all([isinstance(value, (str, int, float)) for value in obj])
-        if is_primitive:
-            key = ".".join(parent_key_list)
-            result[key] = obj
-        else:
-            for i, value in enumerate(obj):
-                get_state_ids(value, [*parent_key_list, str(i)], result)
-    return result
-
-
-# TODO: Remove this function ? See issue #230.
-def copy_state(obj: Any) -> Mapping[str, Any]:
-    """Deep copy the object as a attribute name/value pairs dictionary.
-
-    :param obj:
-    :return:
-    """
-    kwargs = {}
-    for key, _ in obj.__getstate__().items():
-        obj_att = getattr(obj, key)
-        if obj_att is None:
-            cpy_obj = None
-        elif hasattr(
-            obj_att, "copy"
-        ):  # TODO: PYXEL specific: this should be replaced with __copy__
-            cpy_obj = obj_att.copy()
-        else:
-            cpy_obj = type(obj_att)(obj_att)
-        kwargs[key] = cpy_obj
-    return kwargs
