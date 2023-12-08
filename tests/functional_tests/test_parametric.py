c16a3773b9b16512b821e19f22258cbb469f28ed
Frederic Lemmel <frederic.lemmel@ext.esa.int>
2023-12-08 10:29:18 +0100

    Move method 'Observation.debug_parameters' and function 'get_value' to 'test_parametric.py'.

--- tests/functional_tests/test_parametric.py
+++ tests/functional_tests/test_parametric.py
@@ -5,8 +5,10 @@
 #  this file, may be copied, modified, propagated, or distributed except according to
 #  the terms contained in the file ‘LICENCE.txt’.
 
+import logging
 from collections import abc
 from pathlib import Path
+from typing import Any
 
 import numpy as np
 import pytest
@@ -17,6 +19,7 @@ from pyxel.detectors import CCD
 from pyxel.exposure import run_exposure_pipeline
 from pyxel.observation import Observation, ParameterMode
 from pyxel.pipelines import DetectionPipeline, Processor
+from pyxel.state import get_obj_att
 
 expected_sequential = [
     (0, [("level", 10), ("initial_energy", 100)]),
@@ -38,6 +41,43 @@ expected_product = [
     (7, [("level", 30), ("initial_energy", 200)]),
     (8, [("level", 30), ("initial_energy", 300)]),
 ]
+
+
+def get_value(obj: Any, key: str) -> Any:
+    """Retrieve the attribute value of the object given the attribute dot formatted key chain.
+
+    Example::
+
+        >>> obj = {"processor": {"pipeline": {"models": [1, 2, 3]}}}
+        >>> om.get_value(obj, "processor.pipeline.models")
+        [1, 2, 3]
+
+    The above example works as well for a user-defined object with a attribute
+    objects, i.e. configuration object model.
+    """
+    obj, att = get_obj_att(obj, key)
+
+    if isinstance(obj, dict) and att in obj:
+        value = obj[att]
+    else:
+        value = getattr(obj, att)
+
+    return value
+
+
+def debug_parameters(observation: Observation, processor: Processor) -> list:
+    """List the parameters using processor parameters in processor generator."""
+    result = []
+    processor_generator = observation._processors_it(processor=processor)
+    for i, (proc, _, _) in enumerate(processor_generator):
+        values = []
+        for step in observation.enabled_steps:
+            _, att = get_obj_att(proc, step.key)
+            value = get_value(proc, step.key)
+            values.append((att, value))
+        logging.debug("%d: %r", i, values)
+        result.append((i, values))
+    return result
 
 
 @pytest.mark.parametrize(
@@ -69,7 +109,7 @@ def test_pipeline_parametric_without_init_photon(mode: ParameterMode, expected):
     assert isinstance(pipeline, DetectionPipeline)
 
     processor = Processor(detector=detector, pipeline=pipeline)
-    result = observation.debug_parameters(processor)
+    result = debug_parameters(observation=observation, processor=processor)
     assert result == expected
 
     detector.photon.array = np.zeros(detector.geometry.shape, dtype=float)
@@ -118,7 +158,7 @@ def test_pipeline_parametric_without_init_photon_deprecated(
     assert isinstance(pipeline, DetectionPipeline)
 
     processor = Processor(detector=detector, pipeline=pipeline)
-    result = observation.debug_parameters(processor)
+    result = debug_parameters(observation=observation, processor=processor)
     assert result == expected
 
     detector.photon.array = np.zeros(detector.geometry.shape, dtype=float)
