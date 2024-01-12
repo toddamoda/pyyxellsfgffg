8b8c3290a74c824a3e3deab33cd27924019cf78a
constanze.seibert <constanze.seibert@esa.int>
2024-01-12 13:23:33 +0100

    Update copyright.

--- continuous_integration/scripts/create_json_schema.py
+++ continuous_integration/scripts/create_json_schema.py
@@ -1,6 +1,6 @@
 """Create JSON Schema."""
 
-#  Copyright (c) European Space Agency, 2017, 2018, 2019, 2020, 2021, 2022.
+#  Copyright (c) European Space Agency, 2017.
 #
 #  This file is subject to the terms and conditions defined in file 'LICENCE.txt', which
 #  is part of this Pyxel package. No part of the package, including
@@ -691,7 +691,7 @@ def generate_detectors() -> Iterator[str]:
 
 def generate_all_models() -> Iterator[str]:
     lst = get_model_group_info()
-    yield "#  Copyright (c) European Space Agency, 2017, 2018, 2019, 2020, 2021, 2022."
+    yield "#  Copyright (c) European Space Agency, 2017."
     yield "#"
     yield "#  This file is subject to the terms and conditions defined in file 'LICENCE.txt', which"
     yield "#  is part of this Pyxel package. No part of the package, including"
