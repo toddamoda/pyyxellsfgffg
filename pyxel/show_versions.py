d64bcf2eda0f38bbe74a2078107c7e4299bcbd6a
Frederic Lemmel <frederic.lemmel@ext.esa.int>
2024-02-01 10:56:11 +0100

    Remove warnings when using function 'pyxel.show_versions()'.

--- pyxel/show_versions.py
+++ pyxel/show_versions.py
@@ -12,6 +12,7 @@ import os
 import platform
 import struct
 import sys
+import warnings
 from collections.abc import Mapping
 from typing import Any, Optional
 
@@ -157,8 +158,12 @@ def show_versions():
 
     for module_name in dependencies_lst:
         try:
-            # Try to get a module
-            module = importlib.import_module(module_name)
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+
+                # Try to get a module
+                module = importlib.import_module(module_name)
+
         except Exception:
             dependencies[module_name] = None
         else:
