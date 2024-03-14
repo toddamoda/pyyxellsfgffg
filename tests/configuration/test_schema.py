f7df1ee08494677b3010368dee8372a165e35912
Frederic Lemmel <frederic.lemmel@ext.esa.int>
2024-03-14 15:21:13 +0100

    Fix issue with unit tests.

--- tests/configuration/test_schema.py
+++ tests/configuration/test_schema.py
@@ -6,10 +6,13 @@
 #  the terms contained in the file ‘LICENCE.txt’.
 
 import json
+from importlib import resources
 from pathlib import Path
 
 import pytest
 from yaml import safe_load
+
+import pyxel.static
 
 # This is equivalent to 'import jsonschema'
 jsonschema = pytest.importorskip(
@@ -20,8 +23,7 @@ jsonschema = pytest.importorskip(
 
 @pytest.fixture
 def schema(request: pytest.FixtureRequest) -> dict:
-    filename: Path = request.path.parent / "../../static/pyxel_schema.json"
-    full_filename = filename.resolve(strict=True)
+    full_filename: Path = resources.files(pyxel.static).joinpath("pyxel_schema.json")
 
     with full_filename.open() as fh:
         content = json.load(fh)
