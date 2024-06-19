27cccd01eb17f0571c6a7d9d03d3f83143e8176f
Frederic Lemmel <frederic.lemmel@ext.esa.int>
2024-06-19 10:28:44 +0900

    Import 'DataTree' from 'xarray.core.datatree' when it's possible.

--- tests/models/data_processing/test_snr.py
+++ tests/models/data_processing/test_snr.py
@@ -8,7 +8,13 @@
 import numpy as np
 import pytest
 import xarray as xr
-from datatree import DataTree
+
+# Import 'DataTree'
+try:
+    from xarray.core.datatree import DataTree
+except ImportError:
+    from datatree import DataTree  # pip install xarray-datatree
+
 
 from pyxel.detectors import (
     CCD,
