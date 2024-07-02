d49a4c8b1165bcb1a1e53ccba82c97927647a2d4
Frederic Lemmel <frederic.lemmel@ext.esa.int>
2024-07-02 16:22:01 +0200

    Add new function 'pyxel.inputs.load_header'.

--- pyxel/inputs/loader.py
+++ pyxel/inputs/loader.py
@@ -12,7 +12,7 @@ from collections.abc import Sequence
 from contextlib import suppress
 from io import BytesIO, StringIO
 from pathlib import Path
-from typing import Optional, Union
+from typing import TYPE_CHECKING, Optional, Union
 
 import fsspec
 import numpy as np
@@ -22,6 +22,21 @@ from numpy.typing import DTypeLike
 from PIL import Image
 
 from pyxel.options import global_options
+
+if TYPE_CHECKING:
+    from astropy.io import fits
+
+
+def load_header(filename: Union[str, Path]) -> Optional["fits.Header"]:
+    from astropy.io import fits
+
+    if Path(filename).suffix == ".fits":
+        header: fits.Header = fits.getheader(filename)
+
+        return header
+
+    else:
+        return None
 
 
 def load_image(filename: Union[str, Path]) -> np.ndarray:
