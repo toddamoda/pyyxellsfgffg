6038ad9c87a60a7c795ef2bc4b15820463479660
Michael Hubbard <michael.hubbard@open.ac.uk>
2024-09-13 15:55:53 +0100

    Fix type in comment

--- pyxel/models/charge_transfer/cdm.py
+++ pyxel/models/charge_transfer/cdm.py
@@ -213,7 +213,7 @@ def run_cdm_parallel(
     kdim_p = len(nt)
     # np.clip(s, 0., fwc, s)      # full well capacity
     # BUGFIX 20240913 line removed below. nt is already in units of  traps / cm^-3
-    # nt = nt / vg  # serial trap density (traps / cm**3)
+    # nt = nt / vg  # parallel trap density (traps / cm**3)
     # nt_p *= rdose             # absolute trap density [per cm**3]
 
     # IMAGING (non-TDI) MODE
